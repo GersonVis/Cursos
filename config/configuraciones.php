1a6fe6050ec84c4f5a7501f001e8c58e460caadf
GersonVis <visoso126@gmail.com>
2022-05-07 17:15:29 -0500

    cambios en la base de datos

--- config/configuraciones.php
+++ config/configuraciones.php
@@ -1,10 +1,10 @@
 <?php
  define("URL", "http://curso.local.com/");
 
- define("HOST", 'http://localhost/');
+ define("HOST", 'http://us-cdbr-east-05.cleardb.net/');
  define('DB', 'tecnologico');
- define('USER', 'tecnologico');
- define('PASSWORD', 'tecnologico');
+ define('USER', 'b93f5124ff1943');
+ define('PASSWORD', '040a6906');
  define('CHARSET', 'utf8mb4');
  define('RUTAPUBLICA', 'public/documentacion/')
 ?>
