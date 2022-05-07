2160facac1b17241a61db2880187012837adedd0
GersonVis <visoso126@gmail.com>
2022-05-07 17:39:26 -0500

    cambios en la base de datos

--- libs/controller.php
+++ libs/controller.php
@@ -4,14 +4,14 @@
        {
            $this->rutaPublica=constant('RUTAPUBLICA');
            $this->view=new View();
-           session_start();
+         /*  session_start();
           // echo var_dump($_SESSION);
            if($check){
             if(!isset($_SESSION['nombre']) || !isset($_SESSION['clave']) || !isset($_SESSION['idRol'])){
                header('location: /session');
                exit();
             } 
-           }
+           }*/
        }
        function CargarModelo($modelo){
            $url = "models/$modelo"."Modelo.php";
