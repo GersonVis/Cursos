a4d40a85a7bc232597e82d7291b243b6bcc1ea22
GersonVis <visoso126@gmail.com>
2022-05-07 18:58:50 -0500

    cambios arreglando la base

--- config/configuraciones.php
+++ config/configuraciones.php
@@ -1,7 +1,7 @@
 <?php
  define("URL", "http://curso.local.com/");
 
- define("HOST", 'http://us-cdbr-east-05.cleardb.net/');
+ define("HOST", 'us-cdbr-east-05.cleardb.net');
  define('DB', 'heroku_e2c843d308559bc');
  define('USER', 'b93f5124ff1943');
  define('PASSWORD', '040a6906');
