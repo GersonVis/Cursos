25609835af2b5272ea82e6473256090b1cff013c
GersonVis <visoso126@gmail.com>
2022-05-08 17:13:35 -0500

    momo

--- config/configuraciones.php
+++ config/configuraciones.php
@@ -2,7 +2,7 @@
  define("URL", "http://curso.local.com/");
 
  define("HOST", 'us-cdbr-east-05.cleardb.net');
- define('DB', 'heroku_e2c843d308559bc');
+ define('DB', 'heroku_76694ac870e37f5');
  define('USER', 'b0ece9b36dca58');
  define('PASSWORD', 'f61a3e5d');
  define('CHARSET', 'utf8mb4');
