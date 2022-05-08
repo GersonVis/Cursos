9348cf086fe135995c68cd6c987579bbe4908f3d
GersonVis <visoso126@gmail.com>
2022-05-08 00:13:36 -0500

    rama para pruebas remotas

--- controllers/session.php
+++ controllers/session.php
@@ -26,39 +26,8 @@ class Session extends Controller
     
     function registrar()
     {
-        echo var_dump($_POST);
         
-       /* $usuario = $_POST['usuario'];
-        $clave = isset($_POST['clave']) ? $_POST['clave'] : "";
-        if ($usuario != "" && $clave != "") {
-            $respuesta = $this->modelo->usuario($usuario, $clave);
-            // echo var_dump($respuesta);
-            // $registro=$respuesta->fetch_assoc();
-            if ($respuesta) {
-                //si es la primera vez en iniciar session redirige a poder cambiar su nombre de usuario y contraseña
-
-                $_SESSION['nombre'] = $respuesta['nombre'];
-                $_SESSION['id'] = $respuesta['id'];
-                $_SESSION['clave'] = $respuesta['clave'];
-                $_SESSION['idRol'] = $respuesta['idRol'];
-                $_SESSION['idEnlazado'] = $respuesta['idEnlazado'];
-
-                $this->crearCarpetaUsuario($_SESSION['id']);
-                if ($respuesta['nuevo'] == "0" &&  $_SESSION['idRol'] == 3) {
-                    $this->view->mensaje = "Es la primera vez que inicias sesión con nosotros, elige un nuevo nombre de usuario y una contraseña";
-                    $this->mostrarActualizacion();
-                    //header("Location: /session/mostrarActualizacion");
-                    exit();
-                }
-                header('Location: /main');
-                exit();
-            }*/
-        }
-        /*$this->view->mensaje="Datos incorrectos, prueba de nuevo";
-        $this->view->estilo = "colorError";
-        $this->Renderizar("session/index");
-        exit();*/
-
+        echo var_dump($_POST);
 
         // header('location: /main');
     }
@@ -169,3 +138,36 @@ class Session extends Controller
         echo "actualizado correctamente";
     }
 }
+
+
+/*$usuario = $_POST['usuario'];
+        $clave = isset($_POST['clave']) ? $_POST['clave'] : "";
+        if ($usuario != "" && $clave != "") {
+            $respuesta = $this->modelo->usuario($usuario, $clave);
+            // echo var_dump($respuesta);
+            // $registro=$respuesta->fetch_assoc();
+            if ($respuesta) {
+                //si es la primera vez en iniciar session redirige a poder cambiar su nombre de usuario y contraseña
+
+                $_SESSION['nombre'] = $respuesta['nombre'];
+                $_SESSION['id'] = $respuesta['id'];
+                $_SESSION['clave'] = $respuesta['clave'];
+                $_SESSION['idRol'] = $respuesta['idRol'];
+                $_SESSION['idEnlazado'] = $respuesta['idEnlazado'];
+
+                $this->crearCarpetaUsuario($_SESSION['id']);
+                if ($respuesta['nuevo'] == "0" &&  $_SESSION['idRol'] == 3) {
+                    $this->view->mensaje = "Es la primera vez que inicias sesión con nosotros, elige un nuevo nombre de usuario y una contraseña";
+                    $this->mostrarActualizacion();
+                    //header("Location: /session/mostrarActualizacion");
+                    exit();
+                }
+                header('Location: /main');
+                exit();
+            }
+        }
+        $this->view->mensaje="Datos incorrectos, prueba de nuevo";
+        $this->view->estilo = "colorError";
+        $this->Renderizar("session/index");
+        exit();
+*/
