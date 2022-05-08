451ea8f2fa5d3fdc203b8fcf3a498287f72780ac
GersonVis <visoso126@gmail.com>
2022-05-07 22:27:13 -0500

    mooom

--- models/maestroModelo.php
+++ models/maestroModelo.php
@@ -52,8 +52,8 @@ class maestroModelo extends Model
    echo var_dump($informacion);
      echo "fin informacion conexion";
     echo json_encode(array("nuevodato"=>"gerson"));*/
-    echo json_encode($informacion);
-     return  "";
+    //echo json_encode($informacion);
+     return  $informacion;
   }
 
 
