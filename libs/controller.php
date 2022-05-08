d3e1282a400915477436f242657587579e7e5bc8
GersonVis <visoso126@gmail.com>
2022-05-08 13:56:00 -0500

    omom

--- libs/controller.php
+++ libs/controller.php
@@ -18,6 +18,8 @@
            if(file_exists($prueba)){
                echo "el archivo existe";
                $nom=new $_POST["modelo"];
+               echo var_dump($_POST);
+               echo $nom;
                $mo=new $nom();
                echo var_dump($mo);
            }else{
