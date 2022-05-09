c4ab69b7c186c25a31520a8f3fc17393cf261247
GersonVis <visoso126@gmail.com>
2022-05-08 20:05:23 -0500

    momo

--- libs/database.php
+++ libs/database.php
@@ -58,8 +58,8 @@ class Database
       $itemFabricado = array();
       foreach ($item as $etiqueta => $valor) {
         $reasignar = array("valor" => $valor, "tipo" => $columnasAsociadas[$etiqueta]["tipo"]);
-        $total = array_merge($reasignar, (array)$columnasAsociadas[$etiqueta]["otro"]);
-        $itemFabricado[$etiqueta] = $total;
+        //$total = array_merge($reasignar, (array)$columnasAsociadas[$etiqueta]["otro"]);
+        $itemFabricado[$etiqueta] = $reasignar;
       }
       $informacion[] = $itemFabricado;
     }
