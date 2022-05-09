27a290761e529b9eea1e4188479114efcf6d698e
GersonVis <visoso126@gmail.com>
2022-05-08 21:03:52 -0500

    reparando js

--- controllers/maestro.php
+++ controllers/maestro.php
@@ -119,14 +119,15 @@ class Maestro extends Controller{
         
     }
     function carreras(){
-        $respuesta=$this->modelo->carreras();
+        echo var_dump($this->modelo);
+       /* $respuesta=$this->modelo->carreras();
         if($respuesta){
             echo json_encode($respuesta);
             exit();
         }
         json_encode(array("Error"=>"Ocurrio un errror"));
         http_response_code(404);
-        exit();
+        exit();*/
     }
 }
 
