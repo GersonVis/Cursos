cc5e66f40706c910cb7830445ed9b35555f5ba10
GersonVis <visoso126@gmail.com>
2022-05-23 21:29:20 -0500

    final

--- config/configuraciones.php
+++ config/configuraciones.php
@@ -1,10 +1,10 @@
 <?php
  define("URL", "http://curso.local.com/");
 
- define("HOST", 'localhost');
- define('DB', 'tecnologico');
- define('USER', 'root');
- define('PASSWORD', '1234');
+ define("HOST", 'us-cdbr-east-05.cleardb.net');
+ define('DB', 'heroku_76694ac870e37f5');
+ define('USER', 'b0ece9b36dca58');
+ define('PASSWORD', 'f61a3e5d');
  define('CHARSET', 'utf8mb4');
  define('RUTAPUBLICA', 'public/documentacion/')
 ?>
