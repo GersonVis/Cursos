47fd08eb0c572e53e9b03207801c2a5bcf200496
GersonVis <visoso126@gmail.com>
2022-06-05 23:31:07 -0500

    with '#' will be ignored, and an empty message aborts the commit.
    sh: x: command not found

--- config/configuraciones.php
+++ config/configuraciones.php
@@ -7,4 +7,13 @@
  define('PASSWORD', 'f61a3e5d');
  define('CHARSET', 'utf8mb4');
  define('RUTAPUBLICA', 'public/documentacion/')
+
+ /*define("URL", "http://curso.local.com/");
+
+ define("HOST", 'localhost');
+ define('DB', 'tecnologico');
+ define('USER', 'root');
+ define('PASSWORD', '1234');
+ define('CHARSET', 'utf8mb4');
+ define('RUTAPUBLICA', 'public/documentacion/')*/
 ?>
